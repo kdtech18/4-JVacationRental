f883b0a23d7d3a4e9495e6ab04e723c1b6993e8b
kdtech18 <kdtech18@gmail.com>
2016-12-12 13:00:31 -0500

    adding main frame

--- src/JVacationRental.java
+++ src/JVacationRental.java
@@ -5,6 +5,39 @@
    12/12/2016
 */
 
-public class JVacationRental
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JVacationRental extends JFrame implements ActionListener
 	{
+		JPanel mainPnl = new JPanel();
+		JPanel eastPnl = new JPanel();
+		JPanel centerPnl = new JPanel();
+		JPanel westPnl = new JPanel();
+		JPanel southPnl = new JPanel();
+		
+		int totalRent = 0;
+		JLabel totalPrice = new JLabel("Your total rent per week is: " + totalRent);
+		
+		BorderLayout mainLayout = new BorderLayout();
+		
+	
+	public JVacationRental()
+		{
+			super("Lambert's Vacation Rentals");
+			
+		}
+	
+	public void actionPerformed(ActionEvent e)
+		{
+			
+		}
+	
+	public static void main(String[] args)
+		{
+		
+		}// end main method
+	
 	}
